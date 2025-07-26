24dc9f1cdd34ad862b4c8c59049062c55520e449
Matheus Silva <matheus.moraes09@hotmail.com>
2025-07-26 19:13:36 -0300

    Corrigindo variavel de ambiente

--- clima-front/src/services/api.jsx
+++ clima-front/src/services/api.jsx
@@ -1,7 +1,7 @@
 export default function api_options() {
   return {
     params:{
-      Api_Url:process.env.REACT_APP_URL 
+      Api_Url:import.meta.env.VITE_APP_URL 
       //variavel de ambiente no ambiente de hospedagem (vercel por exemplo), 
       //essa variavel inclui a dominio da api no Azure
     }
