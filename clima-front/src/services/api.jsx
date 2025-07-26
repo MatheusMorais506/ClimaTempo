a8a124363fedc1bf57ec3db5291517fb3956d821
Matheus Silva <matheus.moraes09@hotmail.com>
2025-07-26 18:57:06 -0300

    Corrigindo bug vercel

--- clima-front/src/services/api.jsx
+++ clima-front/src/services/api.jsx
@@ -1,7 +1,9 @@
 export default function api_options() {
   return {
     params:{
-      Api_Url:process.env.REACT_APP_URL
+      Api_Url:process.env.REACT_APP_URL 
+      //variavel de ambiente no ambiente de hospedagem (vercel por exemplo), 
+      //essa variavel inclui a dominio da api no Azure
     }
   };
 }
